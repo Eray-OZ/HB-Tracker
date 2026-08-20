29253c8a1398f5845b886b8f602ea5faaa0aa481
Eray-OZ <eray.py@gmail.com>
2026-08-20 16:24:40 +0300

    Worker Host Build

--- HBTracker.Worker/Program.cs
+++ HBTracker.Worker/Program.cs
@@ -1,1 +1,45 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using HBTracker.Data.Context;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+
+
+
+HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+if (string.IsNullOrEmpty(connectionString))
+{
+    throw new ArgumentException("Connection String Cannot Be Null");
+}
+
+builder.Services.AddDbContext<HBTrackerDbContext>(options => 
+options.UseNpgsql(connectionString));
+
+
+using IHost app = builder.Build();
+
+
+using IServiceScope scope = app.Services.CreateScope();
+
+var dbContext =
+    scope.ServiceProvider.GetRequiredService<HBTrackerDbContext>();
+
+try
+{
+    bool canConnect = await dbContext.Database.CanConnectAsync();
+
+    Console.WriteLine(
+        canConnect
+            ? "Database connection successful."
+            : "Database connection failed.");
+}
+catch (Exception exception)
+{
+    Console.WriteLine("Database connection failed.");
+    Console.WriteLine(exception.Message);
+}
