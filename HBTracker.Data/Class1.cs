597434db92aafa90848cb29c396c42728b1c27a0
Eray-OZ <eray.py@gmail.com>
2026-08-20 15:46:56 +0300

    Entities and DbContext

--- HBTracker.Data/Class1.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace HBTracker.Data;
-
-public class Class1
-{
-
-}
---
+++ HBTracker.Data/Context/HBTrackerDbContext.cs
@@ -0,0 +1,12 @@
+﻿using HBTracker.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace HBTracker.Data.Context;
+
+public class HBTrackerDbContext : DbContext
+{
+    public HBTrackerDbContext(DbContextOptions<HBTrackerDbContext> options) : base(options) {}
+
+    public DbSet<PriceHistory> PriceHistories { get; set; }
+    public DbSet<TrackedProduct> TrackedProducts { get; set; }
+}
